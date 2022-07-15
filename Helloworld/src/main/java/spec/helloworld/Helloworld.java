6b04301dffc251ec650b09c391f9eee0f79e3389
Korkibuchi <96173795+ KorkibKorkibuchi@users.noreply.github.com>
2022-07-15 13:57:40 +0300

    0.2

--- Helloworld/src/main/java/spec/helloworld/Helloworld.java
+++ Helloworld/src/main/java/spec/helloworld/Helloworld.java
@@ -2,10 +2,13 @@
 
 package spec.helloworld;
 
+import static java.lang.Math.sqrt;
+
 
 public class Helloworld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(sqrt(225));
     }
 }
